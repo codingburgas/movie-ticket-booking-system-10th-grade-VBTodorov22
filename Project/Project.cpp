86d49c5e418c16ab9cd22bae14f3de819858bb38
VBTodorov22 <VBTodorov22@codingburgas.bg>
2025-04-28 21:03:33 +0100

    Added halls class

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -5,6 +5,15 @@
 using namespace std;
 
 
+
+class Hall {
+public:
+    int hallId;
+    int totalSeats;
+
+    Hall(int id, int seats) : hallId(id), totalSeats(seats) {}
+};
+
 class Movie {
 public:
     string title;
