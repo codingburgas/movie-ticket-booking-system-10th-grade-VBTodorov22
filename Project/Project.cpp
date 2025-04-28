e9edd7cfc94357ddb676aa75ffd636586821ad14
VBTodorov22 <VBTodorov22@codingburgas.bg>
2025-04-28 21:05:09 +0100

    Added cinema class

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 
+
+class Cinema {
+public:
+    string name;
+    string city;
+    vector<Hall> halls;
+
+    Cinema(string n, string c) : name(n), city(c) {}
+
+    void addHall(Hall hall) {
+        halls.push_back(hall);
+    }
+};
 
 class Hall {
 public:
